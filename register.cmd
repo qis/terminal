70ebc025b5f69eb4efb27fed2aa0b7f753e7652b
Alexej Harm <alexej.h@xiphos.de>
2021-05-17 10:54:51 +0200

    remove default shell entries

--- register.cmd
+++ register.cmd
@@ -2,7 +2,7 @@
 
 set shell=HKCU\Software\Classes\Directory\Background\shell
 
-set consoletxt=Open with Console
+set consoletxt=Open in Console
 set consoleico=%~dp0icons\console.ico
 set consolecmd=\"%LocalAppData%\Microsoft\WindowsApps\wt.exe\" -p Console -d .
 
@@ -10,10 +10,14 @@ reg add "%shell%\x-console" /ve /d "%consoletxt%" /f
 reg add "%shell%\x-console" /v Icon /d "%consoleico%" /f
 reg add "%shell%\x-console\command" /ve /d "%consolecmd%" /f
 
-set terminaltxt=Open with Terminal
+set terminaltxt=Open in Terminal
 set terminalico=%~dp0icons\terminal.ico
 set terminalcmd=\"%LocalAppData%\Microsoft\WindowsApps\wt.exe\" -p Terminal -d .
 
 reg add "%shell%\x-terminal" /ve /d "%terminaltxt%" /f
 reg add "%shell%\x-terminal" /v Icon /d "%terminalico%" /f
 reg add "%shell%\x-terminal\command" /ve /d "%terminalcmd%" /f
+
+set block=HKLM\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked
+
+reg add "%block%" /v "{9F156763-7844-4DC4-B2B1-901F640F5155}" /d "WindowsTerminal" /f
--- unregister.cmd
+++ unregister.cmd
@@ -4,3 +4,7 @@ set shell=HKCU\Software\Classes\Directory\Background\shell
 
 reg delete "%shell%\x-console" /f
 reg delete "%shell%\x-terminal" /f
+
+set block=HKLM\Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked
+
+reg delete "%block%" /v "{9F156763-7844-4DC4-B2B1-901F640F5155}" /f
